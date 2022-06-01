e3d7ce8f9ec947dfac4b1c682b56dd767636c6ef
Rachel Bailey <rachel.ebailey@yahoo.com>
2022-06-01 18:42:01 -0400

    created register component and form (in login component)  and used async to secure store

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -122,6 +122,21 @@ class LoginTab extends Component {
 }
 
 class RegisterTab extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password:'',
+            firstname:'',
+            lastname:'',
+            email:'',
+            remember: false,
+            imageUrl: baseUrl+ 'images/logo.png'
+            }
+    }
+
     static navigationOptions = {
         title: 'Register',
         tabBarIdon: ({tintColor}) => (
@@ -133,10 +148,90 @@ class RegisterTab extends Component {
         )
     }
 
+    handleRegister() {
+        console.log(JSON.stringify(this.state));
+        if(this.state.remember) {
+            SecureStore.setItemAsync(
+                'userinfo', 
+                JSON.stringify({
+                    username: this.state.username, 
+                    password: this.state.password
+                })
+            ).catch(error => console.log('Could not save user info', error));
+        } else {
+            SecureStore.deleteItemAsync('userinfo').catch(
+                error => console.log('Could not delete user info', error)
+            );
+        }
+    }
+
     render() {
         return (
             <ScrollView>
-
+                <View style={styles.container}>
+                    <Input 
+                        placeholder="Username"
+                        leftIcon={{type: 'font-awesome', name: 'user-o'}}
+                        onChangeText={username=> this.setState({username})}
+                        value={this.state.username}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <Input 
+                        placeholder="Password"
+                        leftIcon={{type: 'font-awesome', name: 'key'}}
+                        onChangeText={password=> this.setState({password})}
+                        value={this.state.password}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <Input 
+                        placeholder="First Name"
+                        leftIcon={{type: 'font-awesome', name: 'user-o'}}
+                        onChangeText={firstname=> this.setState({firstname})}
+                        value={this.state.firstname}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                     <Input 
+                        placeholder="Last Name"
+                        leftIcon={{type: 'font-awesome', name: 'user-o'}}
+                        onChangeText={lastname=> this.setState({lastname})}
+                        value={this.state.lastname}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                     <Input 
+                        placeholder="Email"
+                        leftIcon={{type: 'font-awesome', name: 'envelope-o'}}
+                        onChangeText={email=> this.setState({email})}
+                        value={this.state.email}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <CheckBox 
+                        title= 'Remember Me'
+                        center
+                        checked={this.state.remember}
+                        onPress={() => this.setState({remember: !this.state.remember})}
+                        containerStyle={styles.formCheckbox}
+                    />
+                    <View style={styles.formButton}>
+                        <Button 
+                            onPress={() => this.handleRegister()}
+                            title="Register"
+                            icon={
+                                <Icon
+                                    name='user-plus'
+                                    type='font-awesome'
+                                    color='#fff'
+                                    iconStyle={{marginRight: 10}}
+                                />
+                            }
+                            buttonStyle={{backgroundColor:"#5637DD"}}
+                        />
+                    </View>
+                </View>
             </ScrollView>
         )
     }
