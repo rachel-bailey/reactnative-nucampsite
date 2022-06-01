b813bf69ccfb79f9197d617a0ce780139c6690c5
Rachel Bailey <rachel.ebailey@yahoo.com>
2022-06-01 13:08:18 -0400

    added share icon and share component to campsite info component

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, 
+    Modal, Button, StyleSheet, 
+    Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -62,6 +64,16 @@ function RenderCampsite(props) {
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title, 
+            message: `${title}: ${message} ${url}`,
+            url
+        }, {
+            dialogTitle: 'Share' + title
+        });
+    }
+
     if (campsite) {
         return (
             <Animatable.View 
@@ -87,11 +99,19 @@ function RenderCampsite(props) {
                         />
                         <Icon 
                             type='font-awesome'
-                            name='pencil'
+                            name={'pencil'}
                             color='#5637DD'
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)}
                         />
                     </View>
                 </Card>
